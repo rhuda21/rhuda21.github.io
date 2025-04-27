c3474a80cba6d6d7f8d722e9f33b061ae9583e36
R H <165597973+rhuda21@users.noreply.github.com>
2025-04-27 05:38:44 +0100

    Add files via upload

---
+++ AZ_UBHubOptions.lua
@@ -0,0 +1,17 @@
+local UBHubOT = {}
+
+UBHubOT.DC = "https://discord.gg/pn8xyhuSeV"
+UBHubOT.Endpoint = "https://ubhub.pages.dev/"
+UBHubOT.KeyAPI = "https://dev.azure.com/muhammadrhuda2025/d3834ece-390c-472a-a3c3-ee5cb8695472/_apis/git/repositories/9f7bacb7-c868-4a34-810a-b7b0955ec59e/items?path=/KeySystem/API/DC_API.lua"
+
+
+UBHubOT.Lib = "https://dev.azure.com/muhammadrhuda2025/d3834ece-390c-472a-a3c3-ee5cb8695472/_apis/git/repositories/9f7bacb7-c868-4a34-810a-b7b0955ec59e/items?path=/Library/V2.lua"
+UBHubOT.TestLib = "https://dev.azure.com/muhammadrhuda2025/d3834ece-390c-472a-a3c3-ee5cb8695472/_apis/git/repositories/9f7bacb7-c868-4a34-810a-b7b0955ec59e/items?path=/Library/TestersLib.lua"
+UBHubOT.SaveManager = "https://dev.azure.com/muhammadrhuda2025/d3834ece-390c-472a-a3c3-ee5cb8695472/_apis/git/repositories/9f7bacb7-c868-4a34-810a-b7b0955ec59e/items?path=/Library/SaveManager.lua"
+UBHubOT.InterfaceManager = "https://dev.azure.com/muhammadrhuda2025/d3834ece-390c-472a-a3c3-ee5cb8695472/_apis/git/repositories/9f7bacb7-c868-4a34-810a-b7b0955ec59e/items?path=/Library/InterfaceManager.luau"
+UBHubOT.UBLink = "https://dev.azure.com/muhammadrhuda2025/d3834ece-390c-472a-a3c3-ee5cb8695472/_apis/git/repositories/9f7bacb7-c868-4a34-810a-b7b0955ec59e/items?path=/UBLink/UBLink4Fluent.lua"
+
+UBHubOT.scriptrepo = "https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/"
+UBHubOT.loader = "https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/"
+
+return UBHubOT
